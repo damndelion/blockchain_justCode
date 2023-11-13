458ffbce8a398946240e3d0df769e5f0d3afe873
damndelion <daniarermakhan003@gmail.com>
2023-11-13 10:16:29 +0600

    Swagger for user grpc

--- blockchain/internal/user/controller/http/v1/router.go
+++ blockchain/internal/user/controller/http/v1/router.go
@@ -6,6 +6,7 @@ import (
 	"github.com/evrone/go-clean-template/internal/user/usecase"
 	"github.com/evrone/go-clean-template/pkg/cache"
 	"github.com/evrone/go-clean-template/pkg/logger"
+	_ "github.com/evrone/go-clean-template/pkg/protobuf/userService/gw"
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -21,10 +22,9 @@ func NewUserRouter(handler *gin.Engine, l logger.Interface, u usecase.UserUseCas
 	pprof.Register(handler)
 
 	// Swagger
-	// Swagger
 	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json") // The url pointing to API definition
 	handler.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
-
+	handler.Static("/grpc/swagger", "/Users/daniar/GolandProjects/blockchain/blockchain/pkg/protobuf/userService/gw")
 	// K8s probe
 	handler.GET("/healthz", func(c *gin.Context) { c.Status(http.StatusOK) })
 
